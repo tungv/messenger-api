97d68d556c111e47cb8b778b45a104cd5fe1dd05
Tung Vu <me@tungv.com>
2022-04-24 20:49:29 +0700

    refactor: get all conversations of an account using supabase
    Signed-off-by: Tung Vu <me@tungv.com>

--- pages/api/account/[accountId]/__tests__/conversations.spec.ts
+++ pages/api/account/[accountId]/__tests__/conversations.spec.ts
@@ -45,4 +45,37 @@ describe("/api/account/:id/conversations", () => {
       });
     });
   });
+
+  describe("GET", () => {
+    it("should return all conversations of an account", async () => {
+      const [req, res] = getReqRes({
+        method: "GET",
+        query: {
+          accountId: "1",
+        },
+      });
+
+      await handler(req, res);
+
+      expect(res.json).toHaveBeenCalledWith({
+        rows: [
+          {
+            id: expect.any(String),
+            participants: [
+              { id: "1", name: "Will Smith" },
+              { id: "2", name: "Jada" },
+            ],
+            lastMessage: expect.any(Object),
+          },
+        ],
+        sort: "NEWEST_FIRST",
+
+        // {"sort":"NEWEST_FIRST","lastSeen":"1"}
+        cursor_next: "eyJzb3J0IjoiTkVXRVNUX0ZJUlNUIiwibGFzdFNlZW4iOiIxIn0=",
+
+        // {"sort":"OLDEST_FIRST","lastSeen":"1"}
+        cursor_prev: "eyJzb3J0IjoiT0xERVNUX0ZJUlNUIiwibGFzdFNlZW4iOiIxIn0=",
+      });
+    });
+  });
 });
--- pages/api/account/[accountId]/conversations.ts
+++ pages/api/account/[accountId]/conversations.ts
@@ -1,6 +1,7 @@
 import { getClient } from "components/server/supabase";
 import withDefaultDb from "components/server/withDb";
 import withMethods from "components/server/withMethods";
+import { toBase64 } from "components/toBase64";
 import { ConversationDocument } from "data";
 import { readConversation } from "./conversation/[conversationId]";
 
@@ -48,6 +49,44 @@ export default withDefaultDb(
       const { id } = data[0];
       const conversation = await readConversation(id);
       res.status(200).json(conversation);
+    },
+
+    async GET(req, res) {
+      const accountId = req.query.accountId as string;
+      const pageSize = (req.query.pageSize as string) || "10";
+
+      const supabase = getClient();
+
+      // check for existing conversation
+      const conversations = await supabase
+        .from<ConversationDocument>("conversations")
+        .select("id")
+        .or(`participant_1.eq.${accountId},participant_2.eq.${accountId}`)
+        .order("id", { ascending: false })
+        .limit(Number.parseInt(pageSize));
+
+      if (conversations.error) {
+        console.error(conversations.error);
+        res.status(500).json({ error: conversations.error });
+        return;
+      }
+
+      if (!conversations.data) {
+        res.status(200).json({
+          rows: [],
+          sort: "NEWEST_FIRST",
+        });
+        return;
+      }
+
+      const rows = await Promise.all(conversations.data.map(({ id }) => readConversation(id)));
+
+      res.status(200).json({
+        sort: "NEWEST_FIRST",
+        rows,
+        cursor_next: toBase64(JSON.stringify({ sort: "NEWEST_FIRST", lastSeen: rows[0].id })),
+        cursor_prev: toBase64(JSON.stringify({ sort: "OLDEST_FIRST", lastSeen: rows[rows.length - 1].id })),
+      });
     },
   })
 );
